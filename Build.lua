106cee4d4d0753c041a50934446a9bb43205963b
luogongkai <luogongkai@digisky.com>
2025-02-20 12:07:25 +0800

    #Update update openssl module

--- Build.lua
+++ Build.lua
@@ -13,33 +13,37 @@ workspace "MultiExtend"
     filter "system:windows"
     buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus","/utf-8" }
 	
-group "Render"
-	include "Render/Render.lua"
 
 group ""
 	include "MultiExtend.lua"
+
+group "Render"
+	include "Render/Render.lua"
 
 group "Sandbox"
 	include "MultiSandbox/MultiSandbox.lua"
 	include "MultiTemplate/Template.lua"
 	
 group "Dependencies"
-	include "Dependencies/SDL.lua"
-	include "Dependencies/SDL_image.lua"
-	include "Dependencies/glad.lua"
-	include "Dependencies/glfw.lua"
-	include "Dependencies/glew.lua"
+	include "Dependencies/openssl.lua"
+	include "Dependencies/dlg.lua"
+	include "Dependencies/freetype.lua"
+	include "Dependencies/harfbuzz.lua"
+	include "Dependencies/spdlog.lua"
 	include "Dependencies/imgui.lua"
 	include "Dependencies/libpng.lua"
 	include "Dependencies/zlib.lua"
 
-	include "Dependencies/dlg.lua"
-	include "Dependencies/freetype.lua"
-	include "Dependencies/harfbuzz.lua"
+group "Dependencies/SDL"
+	include "Dependencies/SDL.lua"
+	include "Dependencies/SDL_image.lua"
 	include "Dependencies/SDL_ttf.lua"
 
-	include "Dependencies/spdlog.lua"
-	
+group "Dependencies/OpenGL"
+	include "Dependencies/glad.lua"
+	include "Dependencies/glfw.lua"
+	include "Dependencies/glew.lua"
+
 group "Dependencies/Examples"
 	include "Dependencies/SDL_image_Example.lua"
 	include "Dependencies/dlg_example.lua"
