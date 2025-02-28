5867e19393142dad1c33b520dfca590eecfa6028
luogongkai <luogongkai@digisky.com>
2025-02-28 14:49:42 +0800

    #Update Directory

--- Directory.lua
+++ Directory.lua
@@ -1,5 +1,8 @@
 LocationDir = "%{wks.location}/settings"
 
+DependenciesRoot = "%{wks.location}/Extension/MultiExtend/Dependencies"
+ExtensionRoot = "%{wks.location}/Extension"
+
 OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
 
 EXEDir = "%{wks.location}/bin/"..OutputDir
@@ -7,5 +10,3 @@ LibDir = "%{wks.location}/bin/"..OutputDir
 DynamicDir = "%{wks.location}/bin/"..OutputDir
 TargetDir = "%{wks.location}/bin/" .. OutputDir .. "/%{prj.name}"
 ObjectDir = "%{wks.location}/bin/obj/" .. OutputDir .. "/%{prj.name}"
-
-LocationDir = "%{wks.location}/settings"
